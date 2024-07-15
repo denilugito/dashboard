a13af73c1796ce9a459e6faa81f0851a4a1fdefc
Deni Tri Hasta <denilugito@gmail.com>
2024-07-15 16:54:00 +0700

    commit project structure for dashboard

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-3xl font-bold underline">
+        Deni Hasta!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
